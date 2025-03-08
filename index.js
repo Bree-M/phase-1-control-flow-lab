fdb3577fb09742f8393e6547be4051a8688cd2c7
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-08 21:31:16 +0300

    update 1

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function scuberGreetingForFeet(){
-  
+  if(distance <=400) {
+    return "THis one is on me!";
+    else if (distance>400 && distance <= 2000) {
+      return "That will be twenty bucks.";
+    } else if (distance > 2000 && distance <=2500) {
+      return "I wil gladly take your thirty bucks.";
+} else [
+  return "No can do.";
+}
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+ return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
