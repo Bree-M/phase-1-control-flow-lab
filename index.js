a32e382ffa53c8431046bc7ba08384e8cd7c3878
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-09 16:48:03 +0300

    commit my fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Function to return appropriate response based on distance traveled
-function scuberGreetingForFeet(199) {
+
+function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return "This one is on me!";
   } else if (distance > 400 && distance <= 2000) {
@@ -11,14 +11,12 @@ function scuberGreetingForFeet(199) {
   }
 }
 
-// Function to check if city is NYC using a ternary operator
 function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-// Function to return response based on tip amount using a switch statement
 function switchOnCharmFromTip(tip) {
-  switch (tip) {
+  switch (tip.toLowerCase()) {
     case "generous":
       return "Thank you so much.";
     case "not as generous":
